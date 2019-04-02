2c9aaf9869fa289e9a3d93612cdd5695be85b9df
SantiagoBeltran <ss.beltran@uniandes.edu.co>
2019-04-02 12:36:45 +0000

    Agregar marcha aleatoria con C++

---
+++ marcha.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdlib.h>
+#include <cmath>
+
+int main(){
+  int i;
+  float x;
+  float y;   
+  float r;
+  double pi;
+  
+  pi=2*asin(1);  
+  x=0;
+  y=0;    
+  srand48(17);
+
+  for (i=0;i<1000;i++){  
+    r = 2*pi*drand48();
+    x+=cos(r);
+    y+=sin(r);
+    std::cout << x << " " << y << std::endl;    
+  }
+  
+  return 0;
+}
